133d7c6d1813b9685aa70bce9cd47bf6c7093ef1
Stephen Everett <alex.alexeverett@gmail.com>
2023-04-05 14:56:54 -0700

    Working with CMake to make development a little easier

---
+++ CMakeFiles/Main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/l0fi/Documents/SDL2/Engine_0002/src/BusNode.cpp" "CMakeFiles/Main.dir/src/BusNode.cpp.o" "gcc" "CMakeFiles/Main.dir/src/BusNode.cpp.o.d"
+  "/home/l0fi/Documents/SDL2/Engine_0002/src/Button.cpp" "CMakeFiles/Main.dir/src/Button.cpp.o" "gcc" "CMakeFiles/Main.dir/src/Button.cpp.o.d"
+  "/home/l0fi/Documents/SDL2/Engine_0002/src/Entity.cpp" "CMakeFiles/Main.dir/src/Entity.cpp.o" "gcc" "CMakeFiles/Main.dir/src/Entity.cpp.o.d"
+  "/home/l0fi/Documents/SDL2/Engine_0002/src/EventBus.cpp" "CMakeFiles/Main.dir/src/EventBus.cpp.o" "gcc" "CMakeFiles/Main.dir/src/EventBus.cpp.o.d"
+  "/home/l0fi/Documents/SDL2/Engine_0002/src/Main.cpp" "CMakeFiles/Main.dir/src/Main.cpp.o" "gcc" "CMakeFiles/Main.dir/src/Main.cpp.o.d"
+  "/home/l0fi/Documents/SDL2/Engine_0002/src/Map.cpp" "CMakeFiles/Main.dir/src/Map.cpp.o" "gcc" "CMakeFiles/Main.dir/src/Map.cpp.o.d"
+  "/home/l0fi/Documents/SDL2/Engine_0002/src/Mouse.cpp" "CMakeFiles/Main.dir/src/Mouse.cpp.o" "gcc" "CMakeFiles/Main.dir/src/Mouse.cpp.o.d"
+  "/home/l0fi/Documents/SDL2/Engine_0002/src/RenderSystem.cpp" "CMakeFiles/Main.dir/src/RenderSystem.cpp.o" "gcc" "CMakeFiles/Main.dir/src/RenderSystem.cpp.o.d"
+  "/home/l0fi/Documents/SDL2/Engine_0002/src/ResourceManager.cpp" "CMakeFiles/Main.dir/src/ResourceManager.cpp.o" "gcc" "CMakeFiles/Main.dir/src/ResourceManager.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
